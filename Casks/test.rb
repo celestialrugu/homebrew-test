61760180218b48286671c5c4dcef4a0e4bb18f3f
celestialrugu <130569498+celestialrugu@users.noreply.github.com>
2023-06-02 10:48:03 +0530

    downgrade version from 6767 to 6756

--- Casks/test.rb
+++ Casks/test.rb
@@ -1,6 +1,6 @@
 cask "test" do
-  version "0.2-6767"
-  sha256 "a64134642a23a32c29d4440bfd9e5de8947c2a3c09ad8b931daa62e1b76cdda2"
+  version "0.2-6756"
+  sha256 "a792e5526e27d7149ec0569a98bdf507da7f001ce3cfc5bebd2e6d05de7c9911"
 
   url "https://github.com/accupara/crave/releases/download/#{version}/crave-#{version}-darwin-amd64.bin"
   name "Crave"
