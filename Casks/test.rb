ef70069099e945df7e4fce0e7b935fc5f2ff83cb
celestialrugu <130569498+celestialrugu@users.noreply.github.com>
2023-06-02 10:40:46 +0530

    Update with higher version

--- Casks/test.rb
+++ Casks/test.rb
@@ -1,6 +1,6 @@
 cask "test" do
-  version "0.2-6764"
-  sha256 "0e45ec3e5464139075b0ddc3846c71fe077bd826dc885538c3a48e763deb15ce"
+  version "0.2-6767"
+  sha256 "a64134642a23a32c29d4440bfd9e5de8947c2a3c09ad8b931daa62e1b76cdda2"
 
   url "https://github.com/accupara/crave/releases/download/#{version}/crave-#{version}-darwin-amd64.bin"
   name "Crave"
